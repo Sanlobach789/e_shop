9018290d86eca40daac95c5a6680262b143194b3
Penetrati0n <fomenko2002ivan@gmail.com>
2022-07-02 10:38:15 +0300

    added signals for filters

--- e_shop/mainapp/admin.py
+++ e_shop/mainapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.dispatch import receiver
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 
-from mainapp.models import Category, Item, Filter, ItemFilter, CategoryFilter
 from mainapp.forms import CategoryForm
+from mainapp.models import Category, Item, Filter, ItemFilter, CategoryFilter
 
 
 class CategoryFilterInline(admin.TabularInline):
@@ -81,15 +81,42 @@ def add_item_filters_existing(instance: CategoryFilter, **kwargs):
             ItemFilter.objects.create(filter=instance.filter, item=item)
 
 
+@receiver(post_delete, sender=CategoryFilter)
+def remove_item_filters_on_delete_category_filter(instance: CategoryFilter, **kwargs):
+    '''
+    Удаляет фильтры из товаров,
+    которые были удалены из категории
+    '''
+    # Получаем товары в категории
+    items = instance.category.item_set.all()
+    # Удаляем фильтр из товаров категории
+    ItemFilter.objects.filter(filter=instance.filter, item__in=items).delete()
+
+
 @receiver(post_save, sender=Item)
 def add_item_filters_new_item(instance: Item, created: bool, **kwargs):
     '''
     Добавление фильтров категории к новому товарам
     '''
+    # Получаем фильтры категории
+    category_filters = instance.category.categoryfilter_set.select_related('filter')
+        
+    # Если создаем товар
     if created:
-        # Получаем фильтры категорий
-        category_filters = instance.category.categoryfilter_set.select_related('filter')
         for cf in category_filters:
             # Добавляем фильтры товару
             ItemFilter.objects.create(item=instance, filter=cf.filter)
 
+    # NOTE: вынести в pre_save, чтобы проверить,
+    # поменялся ли родитель. Если нет, то не надо
+    # выполнять этот блок кода.
+    # Если меняем что-то в товаре
+    else:
+        # Удаляем из товара фильтры, которых нет в новой категории
+        instance.itemfilter_set.exclude(filter__in=category_filters.values_list('filter')).delete()
+        
+        # Добавляем в товар фильтры, которые есть
+        # в новой категории, но нет в товаре
+        for cf in category_filters.exclude(filter__in=instance.itemfilter_set.values_list('filter')):
+            # Добавляем фильтры товару
+            ItemFilter.objects.create(item=instance, filter=cf.filter)
