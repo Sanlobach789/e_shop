9d5343a37605f49beffe0c53316db78fd10b34f1
Penetrati0n <fomenko2002ivan@gmail.com>
2022-07-02 15:47:32 +0300

    added filter output for get query

--- e_shop/mainapp/serializers.py
+++ e_shop/mainapp/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
 
-from mainapp.models import Category, Filter
+from mainapp.models import Category, Filter, ItemFilter
 
 from drf_yasg.utils import swagger_serializer_method
 
 
+class FilterValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemFilter
+        fields = ['name', 'value']
+
+
 class FilterSerializer(serializers.ModelSerializer):
+    values = serializers.SerializerMethodField()
+
     class Meta:
         model = Filter
         fields = ['key', 'name', 'values']
+
+    @swagger_serializer_method(FilterValueSerializer(many=True))
+    def get_values(self, obj: Filter):
+        values = obj.itemfilter_set.all()
+        return FilterValueSerializer(values, many=True).data
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
@@ -20,7 +33,7 @@ class SubCategorySerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     sub_categories = serializers.SerializerMethodField()
     image = serializers.SerializerMethodField()
-    filters = FilterSerializer(many=True)
+    filters = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
@@ -33,6 +46,17 @@ class CategorySerializer(serializers.ModelSerializer):
 
     def get_image(self, obj: Category):
         return obj.image.url
+
+    @swagger_serializer_method(FilterSerializer(many=True))
+    def get_filters(self, obj: Category):
+        filters = [
+            category_filter.filter
+            for category_filter in (
+                obj.categoryfilter_set
+                .prefetch_related('filter__itemfilter_set')
+            )
+        ]
+        return FilterSerializer(filters, many=True).data
 
     def select_sub_categories(self, pk):
         return Category.objects.filter(parent_category=pk)
