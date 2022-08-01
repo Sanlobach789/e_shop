0e317c2ea7f7c0aa42a923a9f0c2a9b1fbe75f84
Penetrati0n <fomenko2002ivan@gmail.com>
2022-08-01 14:25:22 +0300

    redirect on success activation account

--- e_shop/authapp/views_api.py
+++ e_shop/authapp/views_api.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponseRedirect
 from djoser.views import UserViewSet
+from e_shop.settings import SUCCESS_ACTIVATION_VIEW_URL
 
 
 class ActivateUserViewSet(UserViewSet):
     def activation(self, request, *args, **kwargs):
         request.data.update(kwargs)
-        return super().activation(request, *args, **kwargs)
+        super().activation(request, *args, **kwargs)
+        return HttpResponseRedirect(SUCCESS_ACTIVATION_VIEW_URL)
