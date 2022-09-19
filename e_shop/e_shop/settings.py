381cbd233b1d6495a86f084a0a6b4c0682d5a26b
aleksandrlobach <sanlobach789@gmail.com>
2022-09-19 11:48:50 +0300

    sort deleted

--- e_shop/e_shop/settings.py
+++ e_shop/e_shop/settings.py
@@ -162,6 +162,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 DJOSER = {
@@ -219,7 +221,6 @@ LOGGING = {
         },
     },
 }
-
 
 # Url to page redirect on success activation
 SUCCESS_ACTIVATION_VIEW_URL = '/'
